2fa208c85d168f158af45e06efa2646fd727dc39
Jeroen-1063146 <1063146@hr.nl>
2022-11-21 12:50:33 +0100

    Error handling

--- app.py
+++ app.py
@@ -15,3 +15,15 @@ def index():
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", name=name)
+
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
