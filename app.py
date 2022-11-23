d06ad9453ff2b8625c711c18a269ed0e59961bdb
1062573-Danilo <1062573@hr.nl>
2022-11-23 17:52:24 +0100

    Added a base html file

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
 
-LISTEN_ALL = "0.0.0.0"
-FLASK_IP = LISTEN_ALL
-FLASK_PORT = 81
-FLASK_DEBUG = True
+# LISTEN_ALL = "0.0.0.0"
+#FLASK_IP = LISTEN_ALL
+#FLASK_PORT = 81
+#FLASK_DEBUG = True
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -15,3 +14,11 @@ def index():
 @app.route('/login')
 def login():
     return render_template('login.html')
+    
+app.run(debug=True)
+
+
+    
+
+
+
