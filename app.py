5b205248377d07c3bee61e2b0c560dd125c111e1
Nizar-1012373 <113017403+Nizar-1012373@users.noreply.github.com>
2022-11-24 16:40:57 +0100

    user html aangepast

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-
+# safe =
+# capitalize
+# lower
+# upper
+# title
+# trim
+# striptags
 @app.route("/")
 def index():
-    return render_template("index.html")
+    first_name = "Nizar"
+    stuff = "This is <strong> Bold</strong> Text"
+
+    return render_template("index.html", first_name=first_name, stuff=stuff)
+
+
+@app.route("/users/<name>")
+def user(name):
+    return render_template("user.html", user_name=name)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-@app.route("/user/name")
-def user(name):
-    return render_template("user.html", name=name)
-
-
-app.run(debug=True)
