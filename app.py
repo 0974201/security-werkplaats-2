3d6ba811119b88cb8bbcfff64eb443bc9563a020
Bunyamin-1058754 <1058754@hr.nl>
2022-12-15 12:50:17 +0100

    login app route & html pagina voor login toegevoegd

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import csv
 
 from flask import Flask, render_template, make_response, request
 
+from flask import Flask, render_template, redirect, url_for, request
+
+
 from lib.tablemodel import DatabaseModel
 from lib.demodatabase import create_demo_database
 
@@ -163,6 +166,22 @@ def min_max(table_name=None):
             num1=num1,
             num2=num2,
         )
+
+
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+
 
 
 if __name__ == "__main__":
